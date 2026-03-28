aa3c7165891feafd1d0fd4cb8a805ef44ee60966
musse7kr-sudo <musse7kr@gmail.com>
2026-03-28 11:38:35 +0100

    nsss

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 
 <section class="hero">
     <div class="hero-content">
-        <img src="images/logo.png" alt="The Last of Us logo" class="hero-logo">
+     <img src="images/logo.jpg" alt="The Last of Us logo" class="hero-logo">
         <p>Utforska merch, atmosfär och en värld präglad av överlevnad, förfall och svampinfektion.</p>
         <a href="merch.php" class="btn">Se merch</a>
     </div>
